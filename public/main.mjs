e4b3bd65e77daf389e61ea5d87b63245a38925bc
JaredMathis <jared.mathis@native.consulting>
2022-06-02 11:57:06 -0400

    progress

--- public/main.mjs
+++ public/main.mjs
@@ -6,20 +6,30 @@ export async function main(parent) {
         label: 'Douay-Rheims Version, Challoner Revision',
         key: 'drv'
     }]
-    let {version} = element_select_on_change(parent, versions, on_version_change);
+    let books;
+
+    let {select} = await element_select_on_change(parent, versions, on_version_change);
+    let books_container = element(parent, 'div');
+
     async function on_version_change(select) {
         let bible = await axios.get(`https://wlj-bible-public.web.app/${element_select_value(select)}_parsed.json`)
-        let books = _.uniq(_.map(bible.data, 'book'));
-
-        console.log(books);
+        books = _.uniq(_.map(bible.data, 'book'));
+        await books_refresh();
     }
 
+    async function books_refresh() {
+        await element_select_on_change(books_container, books, on_book_change);
+        async function on_book_change(select) {
+        }
+    }
+
+    await on_version_change(select);
 }
 
 async function element_select_on_change(parent, choices, on_change) {
     let {select} = element_select(parent, choices);
     element_on(select, 'change', on_change)
-    await on_change(select);
+    return {select}
 }
 
 function element_select_value(select) {
@@ -34,8 +44,8 @@ function element_select(parent, versions) {
     let select = element(parent, 'select');
     versions.forEach(version => {
         let option = element(select, 'option');
-        element_html_inner_set(option, version.label);
-        element_attribute_set(option, 'value', version.key);
+        element_html_inner_set(option, version.label || version);
+        element_attribute_set(option, 'value', version.key || version);
     });
     return {
         select
