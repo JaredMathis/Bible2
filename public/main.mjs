fa1a20264c837586e2311a463be8a1595682b8e7
JaredMathis <jared.mathis@native.consulting>
2022-06-03 19:38:44 -0400

    progress

--- public/main.mjs
+++ public/main.mjs
@@ -83,7 +83,20 @@ export async function main(parent) {
         verses_refresh();
     }
 
-    let pattern_1 = ['1', '1110', '1101', '1011', '0111', '10', '01', '100', '0', '0', '0'];
+    let pattern_1 = [
+        '1', 
+        '1110', 
+        '1101', 
+        '1011', 
+        '0111', 
+        '10', 
+        '01', 
+        '100', 
+        '1000', 
+        '0', 
+        '0', 
+        '0'
+    ];
 
     let pattern_select = await element_select(
         parent, pattern_1);
